8e705c67b9af43c70068cc1adc3986ad1e086856
mnqrs <116473798+mnqrs@users.noreply.github.com>
2023-09-25 12:24:59 -0300

    finalização do crud

--- Blog Pessoal/Controllers/PostagemController.cs
+++ Blog Pessoal/Controllers/PostagemController.cs
@@ -14,21 +14,82 @@ namespace Blog_Pessoal.Controllers
         private readonly IPostagemService _postagemService;
         private readonly IValidator<Postagem> _postagemValidator;
 
-        public PostagemController(IPostagemService postagemService, 
+        public PostagemController(IPostagemService postagemService,
             IValidator<Postagem> postagemValidator)
         {
             _postagemService = postagemService;
             _postagemValidator = postagemValidator;
         }
 
-        [HttpGet]
-        public async Task<ActionResult> GettAll()
+        [HttpGet] // get para trazer informação
+        public async Task<ActionResult> GetAll()
         {
             return Ok(await _postagemService.GetAll());
         }
 
-       
+        [HttpGet("{id}")] // variável de caminho "{id}"
+        public async Task<ActionResult> GetById(long id)
+        {
+            var Resposta = await _postagemService.GetById(id);
 
+            if (Resposta is null)
+                return NotFound();
+
+            return Ok(Resposta);
+        }
+
+        [HttpGet("titulo/{titulo}")]
+        public async Task<ActionResult> GetByTitulo(string titulo)
+        {
+            return Ok(await _postagemService.GetByTitulo(titulo));
+
+        }
+
+        [HttpPost] // post para enviar informação
+        public async Task<ActionResult> Create([FromBody] Postagem postagem)
+        {
+            var validarPostagem = await _postagemValidator.ValidateAsync(postagem); //usando o postagem validator, valida o objeto postagem
+
+            if (!validarPostagem.IsValid) //exclamação no começo da operação = sinal de negação
+                return StatusCode(StatusCodes.Status400BadRequest, validarPostagem);
+
+            await _postagemService.Create(postagem);
+
+            return CreatedAtAction(nameof(GetById), new {id = postagem.Id}, postagem);
         
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update([FromBody] Postagem postagem)
+        {
+            if (postagem.Id == 0)
+                return BadRequest("Id da Postagem é inválido");
+
+            var validarPostagem = await _postagemValidator.ValidateAsync(postagem); //usando o postagem validator, valida o objeto postagem
+
+            if (!validarPostagem.IsValid) //exclamação no começo da operação = sinal de negação
+                return StatusCode(StatusCodes.Status400BadRequest, validarPostagem);
+            
+            var Resposta = await _postagemService.Update(postagem); // atualização precisa ser guardada em uma variável
+
+            if (Resposta is null)
+                return NotFound("Postagem não encontrada!");
+
+            return Ok(Resposta);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var BuscaPostagem = await _postagemService.GetById(id); //checar se a postagem existe
+
+            if (BuscaPostagem is null)
+                return NotFound("Postagem não encontrada!");
+
+            await _postagemService.Delete(BuscaPostagem); //recebe o objeto encontrado e armazenado na variavel para deletar
+
+            return NoContent();
+        }
+
     }
 }
--- Blog Pessoal/Service/Implements/PostagemService.cs
+++ Blog Pessoal/Service/Implements/PostagemService.cs
@@ -6,41 +6,68 @@ namespace Blog_Pessoal.Service.Implements
 {
     public class PostagemService : IPostagemService
     {
-        private readonly AppDbContext _context;
-
-        public PostagemService(AppDbContext context)
-        {
-            _context = context;
-        }
-
-        public Task<Postagem?> Create(Postagem postagem)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Delete(Postagem postagem)
-        {
-            throw new NotImplementedException();
-        }
+        private readonly AppDbContext _context; // usa o underline pois é readonly
 
         public async Task<IEnumerable<Postagem>> GetAll()
         {
             return await _context.Postagens.ToListAsync();
         }
 
-        public Task<Postagem?> GetById(long id)
+        public async Task<Postagem?> GetById(long id)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                var Postagem = await _context.Postagens.FirstAsync(i => i.Id == id);
+
+                return Postagem;
+
+            }
+
+            catch
+            {
+                return null;
+            }
         }
 
-        public Task<IEnumerable<Postagem>> GetByTitulo(string titulo)
+        public async Task<IEnumerable<Postagem>> GetByTitulo(string titulo)
         {
-            throw new NotImplementedException();
+            var Postagem = await _context.Postagens
+                                         .Where(p => p.Titulo.Contains(titulo))
+                                         .ToListAsync();
+            return Postagem;
+        }
+        public PostagemService(AppDbContext context)
+        {
+            _context = context;
         }
 
-        public Task<Postagem?> Update(Postagem postagem)
+        public async Task<Postagem?> Create(Postagem postagem)
         {
-            throw new NotImplementedException();
+            await _context.Postagens.AddAsync(postagem);
+            await _context.SaveChangesAsync();
+
+            return postagem;
         }
+                    
+        public async Task<Postagem?> Update(Postagem postagem)
+        {
+            var PostagemUpdate = await _context.Postagens.FindAsync(postagem.Id); //primeiro busca a postagem para conseguir atualizar
+
+            if(PostagemUpdate is null) 
+                return null;
+
+            _context.Entry(PostagemUpdate).State = EntityState.Detached; // não quer persistir o objeto na busca
+            _context.Entry(postagem).State = EntityState.Modified; //objeto atualizado = postagem ao inves de PostagemUpdate
+            await _context.SaveChangesAsync();
+
+            return postagem;
+        }
+        public async Task Delete(Postagem postagem)
+        {
+            _context.Remove(postagem);
+            await _context.SaveChangesAsync();
+        }
+
     }
 }
